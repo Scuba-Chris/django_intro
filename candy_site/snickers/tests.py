199de323aaeb92d6b5d110fe4d23a3b28209f230
Christopher Ceder <cceder@ChristophersAir.localdomain>
2020-01-15 09:54:44 -0800

    tests add and site works

--- candy_site/snickers/tests.py
+++ candy_site/snickers/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SnickersTest(SimpleTestCase):
+
+    def test_home_page(self):
+        url = reverse('home')
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'links.html')
+
+    
+    def test_about_snickers_template(self):
+        url = reverse('about_snickers')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'about_snickers.html')
+        self.assertTemplateUsed(response, 'links.html')
